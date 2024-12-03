4b96860afa8181c6d8979820f2d7ffcd07c6ceec
Pavlo Muts <pavlo.muts@lhsystems.com>
2024-12-03 09:40:04 +0100

    leftover

--- day-1/main.py
+++ day-1/main.py
@@ -30,29 +30,23 @@ for i in range(1, n + 1):
     )
 
 # r_j - room is occupied or not
-# sum(x_ij) <= M * r_j
 
 rooms = []
 for j in range(n):
     rooms.append(model.add_binary_variable(name=f"r_{j}"))
 
-for j in range(1, n + 1):
-    model.add_linear_constraint(
-        mathopt.fast_sum([variables[(i, j)] for i in range(1, n + 1)])
-        <= n * rooms[j - 1]
-    )
 
 for j in range(1, n + 1):
     for row in data.iter_rows(named=True):
         k = row["event1"]
         m = row["event2"]
-        model.add_linear_constraint(variables[(k, j)] + variables[m, j] <= 1)
+        model.add_linear_constraint(variables[(k, j)] + variables[m, j] <= rooms[j - 1])
 
 
 model.minimize(mathopt.fast_sum(rooms))
 
 result = mathopt.solve(
-    model, mathopt.SolverType.HIGHS, params=mathopt.SolveParameters(enable_output=True)
+    model, mathopt.SolverType.GSCIP, params=mathopt.SolveParameters(enable_output=True)
 )
 
 print("Objective value:", result.objective_value())
